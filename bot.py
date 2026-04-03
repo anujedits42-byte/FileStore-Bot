34e5248ba04afd7e5aae4aca51c095c3930ce2a2
allentertainmentsstudio <allentertainmentsstudio@gmail.com>
2026-04-03 13:14:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -51,7 +51,7 @@ def get_indian_time():
 
 
 name ="""
- BY CODEFLIX BOTS
+ BY 𝐀𝐍𝐔𝐉 𝐊𝐔𝐌𝐀𝐑
 """
 
 def get_indian_time():
@@ -91,7 +91,7 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/weebs_support")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/log_channel_a")
         self.LOGGER(__name__).info(f"""       
 
 
@@ -105,7 +105,7 @@ class Bot(Client):
 
         self.set_parse_mode(ParseMode.HTML)
         self.username = usr_bot_me.username
-        self.LOGGER(__name__).info(f"Bot Running..! Made by @Codeflix_Bots")   
+        self.LOGGER(__name__).info(f"Bot Running..! Made by @anujedits76")   
 
         # Start Web Server
         app = web.AppRunner(await web_server())
@@ -113,7 +113,7 @@ class Bot(Client):
         await web.TCPSite(app, "0.0.0.0", PORT).start()
 
 
-        try: await self.send_message(OWNER_ID, text = f"<b><blockquote> Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @Codeflix_Bots</blockquote></b>")
+        try: await self.send_message(OWNER_ID, text = f"<b><blockquote> Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @anujedits76</blockquote></b>")
         except: pass
 
     async def stop(self, *args):
@@ -124,7 +124,7 @@ class Bot(Client):
         """Run the bot."""
         loop = asyncio.get_event_loop()
         loop.run_until_complete(self.start())
-        self.LOGGER(__name__).info("Bot is now running. Thanks to @rohit_1888")
+        self.LOGGER(__name__).info("Bot is now running. Thanks to @anujedits76")
         try:
             loop.run_forever()
         except KeyboardInterrupt:
